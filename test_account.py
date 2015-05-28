5d3363205af85512b4553174813a174add8da348
Ted Lawless <lawlesst@gmail.com>
2015-05-28 09:39:39 -0400

    Use decorator for fixtures.

--- test_account.py
+++ test_account.py
@@ -31,88 +31,81 @@ bul_vcr = vcr.VCR(
 
 @bul_vcr.use_cassette('login-out.yaml')
 def test_login():
-    with vcr.use_cassette('../fixtures/login-out.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        #These should raise errors if login fails.
-        sess.login()
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    #These should raise errors if login fails.
+    sess.login()
+    sess.logout()
 
 
 #For now - this is expected to fail.
 @pytest.mark.xfail
 @bul_vcr.use_cassette('login-bad.yaml')
 def test_login_bad():
-    with vcr.use_cassette('../fixtures/login-bad.yaml', filter_post_data_parameters=['name', 'code']):
-        ( name, barcode ) = ( 'foo', 'bar' )
-        sess = IIIAccount( name, barcode )
-        exception = None
-        try:
-            sess.login()  # should raise error
-        except Exception as e:
-            exception = repr(e)
-        assert exception == 'Login failed.'
+    ( name, barcode ) = ( 'foo', 'bar' )
+    sess = IIIAccount( name, barcode )
+    exception = None
+    try:
+        sess.login()  # should raise error
+    except Exception as e:
+        exception = repr(e)
+    assert exception == 'Login failed.'
 
 
 @bul_vcr.use_cassette('checkouts.yaml')
 def test_get_checkouts():
-    with vcr.use_cassette('../fixtures/checkouts.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        sess.login()
-        checkouts = sess.get_checkouts()
-        assert "Z695.Z8 F373 2010" in [i['call_number'] for i in checkouts]
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    checkouts = sess.get_checkouts()
+    assert "Z695.Z8 F373 2010" in [i['call_number'] for i in checkouts]
+    sess.logout()
 
 
 @bul_vcr.use_cassette('grad-hold-open-circ.yaml')
 def test_complete_hold():
-    with vcr.use_cassette('../fixtures/grad-hold-open-circ.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        sess.login()
-        bib = 'b2305331'
-        # Get the items available for requesting.
-        items = sess.get_items(bib)
-        # Hold an item - the interface would offer a choice
-        item = items[0]['id']
-        hold = sess.place_hold(bib, item)
-        print json.dumps(hold, indent=2)
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    bib = 'b2305331'
+    # Get the items available for requesting.
+    items = sess.get_items(bib)
+    # Hold an item - the interface would offer a choice
+    item = items[0]['id']
+    hold = sess.place_hold(bib, item)
+    print json.dumps(hold, indent=2)
+    sess.logout()
 
 
 @bul_vcr.use_cassette('get-holds.yaml')
 def test_get_holds():
-    with vcr.use_cassette('../fixtures/get-holds.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        sess.login()
-        existing_holds = sess.get_holds()
-        assert existing_holds[0]['key'] == 'canceli11425642x00'
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    existing_holds = sess.get_holds()
+    assert existing_holds[0]['key'] == 'canceli11425642x00'
+    sess.logout()
 
 
 @bul_vcr.use_cassette('cancel-single-hold.yaml')
 def test_cancel_hold():
     cancel_key = 'canceli11425642x00'
-    with vcr.use_cassette('../fixtures/cancel-single-hold.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        sess.login()
-        canceled = sess.cancel_hold(cancel_key)
-        print json.dumps(canceled, indent=2)
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    canceled = sess.cancel_hold(cancel_key)
+    print json.dumps(canceled, indent=2)
+    sess.logout()
 
 
 @bul_vcr.use_cassette('undergrad-hold-denied-open-circ.yaml')
 def test_denied_hold():
-    with vcr.use_cassette('../fixtures/undergrad-hold-denied-open-circ.yaml', filter_post_data_parameters=['name', 'code']):
-        sess = IIIAccount(name, barcode)
-        sess.login()
-        bib = 'b2305331'
-        # Get the items available for requesting.
-        items = sess.get_items(bib)
-        # Hold an item - the interface would offer a choice
-        item = items[0]['id']
-        hold = sess.place_hold(bib, item)
-        assert hold['confirmed'] == False
-        assert hold['message'] == "No requestable items are available"
-        sess.logout()
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    bib = 'b2305331'
+    # Get the items available for requesting.
+    items = sess.get_items(bib)
+    # Hold an item - the interface would offer a choice
+    item = items[0]['id']
+    hold = sess.place_hold(bib, item)
+    assert hold['confirmed'] == False
+    assert hold['message'] == "No requestable items are available"
+    sess.logout()
 
 
 if __name__ == "__main__":
