d744fb5244699c0fff47cabc184bb0fcdf6e88e8
birkin <birkin.diana@gmail.com>
2015-06-03 16:32:29 -0400

    adds test for get_fines()

--- test_account.py
+++ test_account.py
@@ -117,5 +117,14 @@ def test_get_items():
     sess.logout()
 
 
+@bul_vcr.use_cassette('get-fines.yaml')
+def test_get_fines():
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    fines = sess.get_fines()
+    assert [ 'fines', 'total' ] == sorted( fines.keys() )
+    sess.logout()
+
+
 if __name__ == "__main__":
     raise Exception("Run with py.test.")
