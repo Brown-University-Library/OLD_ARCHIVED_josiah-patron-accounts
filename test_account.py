020c4fe785523b10c6c47e05a25cff31f857b917
birkin <birkin.diana@gmail.com>
2015-07-02 14:40:28 -0400

    adds failing test for incorrect annex response.

--- test_account.py
+++ test_account.py
@@ -72,6 +72,20 @@ def test_complete_hold():
     sess.logout()
 
 
+#For now - this is expected to fail.
+@pytest.mark.xfail
+@bul_vcr.use_cassette('grad-hold-open-circ-annex.yaml')
+def test_place_hold_annex():
+    """ Tests known requestable item from Annex. """
+    sess = IIIAccount(name, barcode)
+    sess.login()
+    bib = 'b4069600'
+    item = 'i117883608'  # year 1996, volume 53, from <http://josiah.brown.edu/record=b4069600>
+    hold = sess.place_hold(bib, item)
+    print json.dumps(hold, indent=2)
+    sess.logout()
+
+
 @bul_vcr.use_cassette('get-holds.yaml')
 def test_get_holds():
     sess = IIIAccount(name, barcode)
