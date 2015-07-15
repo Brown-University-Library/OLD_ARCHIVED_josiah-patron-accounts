34c956d817746dcef9d5e02e5bac365c7863f41e
birkin <birkin.diana@gmail.com>
2015-07-15 10:58:16 -0400

    updates version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='josiah-patron-accounts',
-    version='0.3-dev',
+    version='0.4-dev',
     packages=find_packages(),
     install_requires=[ u'beautifulsoup4==4.3.2', u'requests==2.7.0' ],
 )
