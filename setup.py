aaf39e831f35127826a13298cb2ab332ff354118
birkin <birkin.diana@gmail.com>
2015-08-19 09:41:33 -0400

    bumps version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
 setup(name='josiah-patron-accounts',
-    version='0.5-dev',
+    version='0.6-dev',
     packages=find_packages(),
     install_requires=[ 'beautifulsoup4==4.3.2', 'requests==2.7.0' ],
 )
